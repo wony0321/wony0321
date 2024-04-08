0f2125787096d67ad0fc07138ec597fe703a52a8
YaewonLim <lyw1212@gmail.com>
2024-04-08 16:35:17 +0900

    20240408update

---
+++ Django+DB/02-02-many-to-one-relationship/articles/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Article, Comment
+
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        # fields = '__all__'
+        # fields = ('title', 'content',)
+        exclude = ('user',)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
+        # fields = '__all__'
